349615991a79fd81c47db41dde7cdd448b0da93b
Deepikapc <deepu.deepika.pc@gmail.com>
2018-07-17 15:09:22 +0530

    added validation for review comment

--- app/assets/javascripts/movie_show.js
+++ app/assets/javascripts/movie_show.js
@@ -1,17 +1,21 @@
-$( document ).ready(function() {
-  //alert( "ready!" );
-  //Reading stars while showing specific movie
-	$('.star-rating').raty({
-	  path: '/assets/',
-	  readOnly: true,
-	  score: function() {
-	    return $(this).attr('data-score');
-	  }
-	});
+// $( document ).ready(function() {
+// //   alert( "ready!" );
+// //   //Reading stars while showing specific movie
+// 	$('.star-rating').raty({
+// 	  path: '/assets/',
+// 	  readOnly: true,
+// 	  score: function() {
+// 	    return $(this).attr('data-score');
+// 	  }
+// 	});  
 
-	//writing stars while creating review
-	$('#star-rating').raty({  
-	  path: '/assets/',
-	  scoreName: 'review[rating]',
-	});
-});
+  
+// 	$('#star-rating').raty({  
+// 	  path: '/assets/',
+// 	  scoreName: 'review[rating]',
+// 	  showHalf: true
+// 	});  
+
+	
+// });
+
